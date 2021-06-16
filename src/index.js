44b5fea68f5085fb20004d0e125552986aafbcb3
Gus <gsc13@tuta.io>
2021-06-16 14:21:34 -0300

    Add initial project files

---
+++ src/index.js
@@ -0,0 +1,114 @@
+import Phaser from 'phaser';
+import sky from './assets/sky.png';
+import ground from './assets/platform.png';
+import dude from './assets/dude.png';
+import star from './assets/star.png'
+
+let platforms;
+let player;
+let cursors;
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scale: {
+    // Fit to window
+    mode: Phaser.Scale.FIT,
+    // Center vertically and horizontally
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+    }
+  },
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
+
+let game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.image('sky', sky);
+  this.load.image('ground', ground);
+  this.load.image('star', star);
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', dude,
+      { frameWidth: 32, frameHeight: 48 }
+  );
+}
+
+function create ()
+{
+  this.add.image(400, 300, 'sky');
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.1);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+  this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+  });
+
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+  this.physics.add.collider(player, platforms);
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-100);
+    }
+}
