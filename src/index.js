883d447d8b9cb98fd233894161f2017a93e96a3e
Gus <gsc13@tuta.io>
2021-06-16 14:30:37 -0300

    Add linters and Github Action

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import Phaser from 'phaser';
 import sky from './assets/sky.png';
 import ground from './assets/platform.png';
 import dude from './assets/dude.png';
-import star from './assets/star.png'
+import star from './assets/star.png';
 
 let platforms;
 let player;
 let cursors;
 
-let config = {
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -16,37 +16,34 @@ let config = {
     // Fit to window
     mode: Phaser.Scale.FIT,
     // Center vertically and horizontally
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   physics: {
     default: 'arcade',
     arcade: {
-        gravity: { y: 300 },
-        debug: false
-    }
+      gravity: { y: 300 },
+      debug: false,
+    },
   },
   scene: {
-      preload: preload,
-      create: create,
-      update: update
-  }
+    preload,
+    create,
+    update,
+  },
 };
 
-let game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload() {
   this.load.image('sky', sky);
   this.load.image('ground', ground);
   this.load.image('star', star);
   this.load.image('bomb', 'assets/bomb.png');
   this.load.spritesheet('dude', dude,
-      { frameWidth: 32, frameHeight: 48 }
-  );
+    { frameWidth: 32, frameHeight: 48 });
 }
 
-function create ()
-{
+function create() {
   this.add.image(400, 300, 'sky');
 
   platforms = this.physics.add.staticGroup();
@@ -63,52 +60,45 @@ function create ()
   player.setCollideWorldBounds(true);
 
   this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
   });
 
   this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'dude', frame: 4 } ],
-      frameRate: 20
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
   });
 
   this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
   });
 
   this.physics.add.collider(player, platforms);
   cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update ()
-{
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-160);
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
 
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
-        player.setVelocityX(160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
 
-        player.anims.play('right', true);
-    }
-    else
-    {
-        player.setVelocityX(0);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
 
-        player.anims.play('turn');
-    }
+    player.anims.play('turn');
+  }
 
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.setVelocityY(-100);
-    }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-100);
+  }
 }
--- webpack/base.js
+++ webpack/base.js
@@ -1,57 +1,57 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const stylesHandler = 'style-loader';
 const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.js'
-    },
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
 
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-    },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+  },
 
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-        new webpack.DefinePlugin({
-            'typeof CANVAS_RENDERER': JSON.stringify(true),
-            'typeof WEBGL_RENDERER': JSON.stringify(true)
-        }),
-        new CleanWebpackPlugin({
-            root: path.resolve(__dirname, "../")
-        })
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, '../'),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: ['raw-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: ['file-loader'],
+      },
     ],
-
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-                exclude: '/node_modules/'
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: [/\.vert$/, /\.frag$/],
-                use: ['raw-loader']
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg|xml)$/i,
-                use: ['file-loader']
-            }
-        ],
-    },
+  },
 };
