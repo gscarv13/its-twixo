59698287dd1830b357fd7937084568a72a48aa61
Gus <gsc13@tuta.io>
2021-06-18 15:04:01 -0300

    Add obstacles image to the game

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './assets/stylesheet/style.css';
 import Phaser from 'phaser';
-import bgIMG from './assets/Seasonal Tilesets/1 - Grassland/Background parts/_Complete_static_BG_(288 x 208).png';
-import botIMG from './assets/Sprite Pack 5/1 - Robo Retro/Flying_(32 x 32).png';
+import bgIMG from './assets/Environment/bg-static.png';
+import botIMG from './assets/Character/flying.png';
+import obstacle1IMG from './assets/Environment/obstacle1.png';
+import obstacle2IMG from './assets/Environment/obstacle2.png';
 
 const config = {
   type: Phaser.AUTO,
@@ -14,7 +16,6 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
       debug: true,
     },
   },
@@ -26,16 +27,31 @@ const config = {
 };
 
 let bot;
-let initialBotPosition = { x: config.width / 10, y: config.height / 2 };
+let obstacleTop;
+let obstacleBottom;
+
+const obstacleYDistanceRange = [100, 250];
+const obstacleYDistance = Phaser.Math.Between(...obstacleYDistanceRange);
+const obstacleYPosition = Phaser.Math.Between(60, config.height - 60 - obstacleYDistance);
+
+const initialBotPosition = { x: config.width / 10, y: config.height / 2 };
 
 function preload() {
+  console.log(this);
   this.load.image('bg', bgIMG);
   this.load.image('bot', botIMG);
+
+  this.load.image('obstacle1', obstacle1IMG);
+  this.load.image('obstacle2', obstacle2IMG);
 }
 
 function create() {
   this.add.image(0, 0, 'bg').setOrigin(0).setScale(3);
-  bot = this.physics.add.sprite(initialBotPosition.x, initialBotPosition.y, 'bot').setScale(2).setOrigin(0); 
+  obstacleTop = this.physics.add.sprite(400, obstacleYPosition, 'obstacle2').setOrigin(0, 1);
+  obstacleBottom = this.physics.add.sprite(400, obstacleYDistance + obstacleTop.y, 'obstacle1').setOrigin(0);
+
+  bot = this.physics.add.sprite(initialBotPosition.x, initialBotPosition.y, 'bot').setScale(2).setOrigin(0);
+  bot.body.gravity.y = 400;
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
@@ -47,7 +63,7 @@ function resetBotPosition() {
 }
 
 function update() {
-  if (bot.y > config.height || bot.y < bot.height) {
+  if (bot.y > 545 || bot.y < bot.height - 30) {
     resetBotPosition();
     bot.body.velocity.y = 0;
   }
