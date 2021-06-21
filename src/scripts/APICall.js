0d684374e1a03cd71f134a369fa11010fe0cff79
Gus <gsc13@tuta.io>
2021-06-21 15:54:04 -0300

    Add regenerator dependency, Add APICall module

---
+++ src/scripts/APICall.js
@@ -0,0 +1,20 @@
+import 'regenerator-runtime';
+
+async function getScore(url, opt, callback) {
+  opt.method = 'GET';
+  delete opt.body;
+
+  const res = await fetch(url, opt);
+  const data = await res.json();
+  return (callback.bind(this))(data);
+}
+
+async function setScore(url, opt, userData, callback) {
+  opt.method = 'POST';
+  opt.body = JSON.stringify(userData);
+  const res = await fetch(url, opt);
+  const data = await res.json();
+  return callback.apply(this, data);
+}
+
+export { getScore, setScore };
