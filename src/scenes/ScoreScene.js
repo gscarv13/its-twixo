0536fc8ce0522f62912ba9e431c951a1ccfb3ecc
Gus <gsc13@tuta.io>
2021-06-23 13:44:43 -0300

    Update ScoreScene

--- src/scenes/ScoreScene.js
+++ src/scenes/ScoreScene.js
@@ -10,7 +10,7 @@ class ScoreScene extends BaseScene {
     super.create();
 
     this.boardMessage = this.add.text(
-      this.config.width / 2,
+      this.config.width / 2 - 50,
       this.config.height / 2,
       'loading...',
       { ...this.fontOptions, fontSize: '22px' },
@@ -23,6 +23,8 @@ class ScoreScene extends BaseScene {
     let bestScore = input.result;
     bestScore = bestScore.sort((a, b) => b.score - a.score);
     const topScores = bestScore.slice(0, 4);
+    this.add.text(this.screenCenter[0], 100, 'Here are the Top 3 Players:', this.fontOptions).setOrigin(0.5);
+
     this.boardMessage.setText(`
     1st -> ${topScores[0].user}: ${topScores[0].score}\n
     2nd -> ${topScores[1].user}: ${topScores[1].score}\n
